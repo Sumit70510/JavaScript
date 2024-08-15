d6322fcf89fe1daf12e73624482348b2306a86a2
Sumit <sumit.verma.70510@gmail.com>
2024-08-16 03:41:43 +0530

    Next phase Commit

--- GuessNUmber/script.js
+++ GuessNUmber/script.js
@@ -1,57 +1,102 @@
-const Random=parseInt(math.Random()*100+1);
 
-const submit=document.getElementById("#button");
-const userInput=document.getElementById('#field');
-const gussSlot=document.getElementsByClassName(".guesses");
-const remaining=document.getElementsByClassName(".lastResult");
-const lowOrHi=document.getElementsByClassName(".lowOrhigh");
-const startOver=document.getElementsByClassName(".resultParas");
+const submit=document.querySelector("#subt");
+console.log(submit);
+const userInput=document.querySelector('#guessField');
+console.log(userInput);
+const gussSlot=document.querySelector(".guesses");
+const remaining=document.querySelector(".lastResult");
+const lowOrHi=document.querySelector(".lowOrHigh");
+const startOver=document.querySelector(".resultParas");
 
+const rand = parseInt(Math.random() * 100 + 1);
 const p=document.createElement('p');
 
-let gamePlay=1;
+let gamePlay=true;
 let prevGuess= [];
-let numGuess= 1;
+let numGuess= 10;
 
-function play(game)
-{
-  if(game)
+ if(gamePlay&&numGuess>0)
    {
-     submit.addEventListener('click',function(e)
-      {
+     submit.addEventListener('click', function(e)
+       {
+         e.preventDefault();
+         let guess=parseInt(userInput.value); 
         
-      }
-     )
-   }  
-}
+         validate(guess);
+       }
+         );
+   } 
+
+   
 function validate(guess)
 {
-
+   if(!isNaN(guess)&&(guess>0&&guess<101))
+     {  
+         
+         gussSlot.innerHTML="";
+        check(guess);
+     }
+    else 
+     { 
+       gussSlot.innerHTML="Enter A Valid Number";
+     } 
 }
 
 function check(guess)
-{
-
+{   
+    prevGuess.push(guess);
+    gussSlot.innerHTML=`${prevGuess}`;
+   if(guess===rand)
+     {
+       displayGuess("Correct");
+     }
+   else
+    {
+      if(guess>rand)
+         {displayGuess("Higher");}
+      else
+         {displayGuess("Lower");}
+    }  
 }
-
+let win=0;
 function displayGuess(guess)
 {
-
+  if(guess==="Correct")
+     {win=1;displayMessage('Your Guess is '+guess);}
+  else
+    {
+      numGuess-=1;
+      displayMessage('Your Guess is a Bit '+guess);
+    }
 }
 
-function displayMessage(guess) 
+function displayMessage(message) 
 {
-
+  remaining.innerHTML=`${numGuess}`;
+  lowOrHi.innerHTML=`${message}`;
+  newGame(win);
 }
 
-function newGame()
+function newGame(win)
 {
-
+  if(win!==1&&numGuess>0)
+     {
+    //    game(1);
+     }
+   else
+    {  
+      endGame(win);       
+    }  
 }
 
-function endGame()
+function endGame(win)
 {
-
+  const temp=document.createElement("p");  
+  if(win===1)
+   {temp.innerHTML="YOU WIN";}
+  else
+   {temp.innerHTML="GAME OVER";}
+  lowOrHi.append(temp);
 }
 
-play(gamePlay);
+game(gamePlay);
